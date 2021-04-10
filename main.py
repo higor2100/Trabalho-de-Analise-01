8d976f838b03f1323a8fd32243cbe85d0d64d51e
higor2100 <42756331+higor2100@users.noreply.github.com>
2021-04-10 20:04:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
-def maximoEntreListas(vet, inicio, meio, final):
-    soma = 0
-    esq = -float("inf")
-    for i in range(meio, inicio - 1, -1):
-        soma += vet[i]
-        if soma > esq:
-            esq = soma
-            maxEsq = i
-    dirSoma = -float("inf")
-    soma = 0
-    for j in range(meio + 1, final + 1):
-        soma += vet[j]
-        if soma > dirSoma:
-            dirSoma = soma
-            maxDir = j
-    return maxEsq, maxDir, esq + dirSoma
+def maximoEntreListas(v, i, m, f):
+    # i = inicio; f = final, m = meio, v = vetor
+    s = 0
+    #s = Soma
+    e = -float("inf")
+    #e = Esquerda
+    for j in range(m, i - 1, -1):
+        s += v[j]
+        if s > e:
+            e = s
+            mE = j
+            #mE = Maximo Esquerdo
+    dS = -float("inf")
+    #Soma a Direita
+    s = 0
+    for j in range(m + 1, f + 1):
+        s += v[j]
+        if s > dS:
+            dS = s
+            mD = j
+            #Maximo a Direita
+    return mE, mD, e + dS
 
 
-def somaDoMaxima(vet, inicio, final):
-    if inicio == final:
-        return inicio, final, vet[inicio]
+def somaDoMaxima(v, i, f):
+    if i == f:
+        return i, f, v[i]
     else:
-        meio = round((inicio + final) / 2 - 0.5)
-        inicioEsq, finalEsq, esq = somaDoMaxima(vet, inicio, meio)
-        inicioDir, finalDir, somaDir = somaDoMaxima(vet, meio + 1, final)
-        inicio, final, soma = maximoEntreListas(vet, inicio, meio, final)
+        m = round((i + f) / 2 - 0.5)
+        iE, fE, e = somaDoMaxima(v, i, m)
+        #iE = inicio a esquerda; fE = final a esquerda, e = esquerda
+        iD, fD, sD = somaDoMaxima(v, m + 1, f)
+        #iD = inicio a direita; fD = final a direita, d = direita
+        i, f, s = maximoEntreListas(v, i, m, f)
 
        
-        if somaDir >= esq and somaDir >= soma:
-            return inicioDir, finalDir, somaDir
-        elif esq >= somaDir and esq >= soma:
-            return inicioEsq, finalEsq, esq
+        if sD >= e and sD >= s:
+            #sD =soma direta
+            return iD, fD, sD
+        elif e >= sD and e >= s:
+            return iE, fE, e
         else:
-            return inicio, final, soma
+            return i, f, s
 
 vec1 = [10, 13, -30, 1, 25, -8, -14, 1]
 vec2 = [-16, 20, -10, 12, 27, -6, -4, 8]
